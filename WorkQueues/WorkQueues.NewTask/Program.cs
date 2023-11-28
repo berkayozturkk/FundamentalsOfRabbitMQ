783627e28df6974ce7818caf52584689cb1a69f2
Berkay Öztürk <73599896+berkayozturkk@users.noreply.github.com>
2023-11-28 22:07:20 +0300

    Work Queues

---
+++ WorkQueues/WorkQueues.NewTask/Program.cs
@@ -0,0 +1,55 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace WorkQueues.NewTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press [enter] to send message");
+            Console.Read();
+
+            ConnectionFactory factory = new();
+
+            factory.HostName = "localhost";
+            factory.Port = 5672;
+            factory.UserName = "guest";
+            factory.Password = "guest";
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(
+                queue: "task_queue",
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null
+            );
+
+            string message = GetMessage(args);
+
+            var body = Encoding.UTF8.GetBytes(message);
+
+            var properties = channel.CreateBasicProperties();
+            properties.Persistent = true;
+
+            channel.BasicPublish(
+                exchange: string.Empty,
+                routingKey: "task_queue",
+                basicProperties: properties,
+                body: body);
+
+            Console.WriteLine($" [*] Send {message}");
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+        static string GetMessage(string[] args)
+        {
+            return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+        }
+    }
+}
---
+++ WorkQueues/WorkQueues.Worker/Program.cs
@@ -0,0 +1,49 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace WorkQueues.Worker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new() { HostName = "localhost" };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(
+                queue: "task_worker",
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null
+             );
+
+            channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+            Console.WriteLine(" [*] Waiting for messages.");
+
+            EventingBasicConsumer consumer = new(channel);
+
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($" [*] Recived : {message}");
+
+                int dots = message.Split(".").Length - 1;
+                Thread.Sleep(dots * 1000);
+
+                Console.WriteLine(" [x] Done");
+            };
+
+            channel.BasicConsume(queue: "task_worker", autoAck: false, consumer: consumer);
+
+            Console.WriteLine("Press [enter] to exit.");
+
+            Console.ReadLine();
+        }
+    }
+}
