f10451e19e6042ca7aee15cb748f8908086a14e0
Berkay Öztürk <73599896+berkayozturkk@users.noreply.github.com>
2023-11-28 21:27:29 +0300

    Add project files.

---
+++ RabbitMQ.Send/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace RabbitMQ.Send
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new();
+
+            factory.HostName = "localhost";
+            factory.Port = 5672;
+            factory.UserName = "guest";
+            factory.Password = "guest";
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare(
+            queue: "hello",
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null
+           );
+
+            string message = "Hello World!";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(
+                exchange: string.Empty,
+                routingKey: "hello",
+                basicProperties: null,
+                body: body);
+
+            Console.WriteLine($" [*] Send {message}");
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
